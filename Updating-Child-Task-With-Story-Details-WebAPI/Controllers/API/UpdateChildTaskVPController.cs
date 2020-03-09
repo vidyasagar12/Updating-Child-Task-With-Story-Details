a97c0bdfa9ebd7ac81c2f9673fcb165c39c93920
Vidyasagar Patangi <vidyasagar.patangi@ecanarys.com>
2020-03-09 12:56:20 +0530

    Commit

--- Updating-Child-Task-With-Story-Details-WebAPI/Controllers/API/UpdateChildTaskVPController.cs
+++ Updating-Child-Task-With-Story-Details-WebAPI/Controllers/API/UpdateChildTaskVPController.cs
@@ -1,32 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Helpers;
+﻿using System.Collections.Generic;
+using System.Web;
 using System.Web.Http;
 using System.Web.Http.Results;
+using Newtonsoft.Json;
+using Updating_Child_Task_With_Story_Details_WebAPI.Models;
 
 namespace Updating_Child_Task_With_Story_Details_WebAPI.Controllers.API
 {
+    [RoutePrefix("api")]
     public class UpdateChildTaskVPController : ApiController
     {
+        static ModelProps props;
         // GET: api/UpdateChildTaskVP
+        [HttpGet]
+        [Route("getItems")]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
-
+        //UpdateChildTaskVP
         // GET: api/UpdateChildTaskVP/5
+        [HttpGet]
+        [Route("getItemsId")]
         public string Get(int id)
         {
             return "value";
         }
-
+        [HttpPost]
+        [Route("postTaskDetails")]
         // POST: api/UpdateChildTaskVP
-        public OkResult Post([FromBody]string value)
+        public OkResult Post()
         {
+            //var requestBody=Request.Content.ReadAsStringAsync().Result;
+         
+            var requestBody = System.IO.File.ReadAllText(HttpContext.Current.Server.MapPath("~/JSON/TaskData"));
+            Task task = JsonConvert.DeserializeObject<Task>(requestBody);
+         
+            props = new ModelProps()
+            {
+                parentId = 1,
+                projectId = "",
+                PAT = ""
+            };
             return Ok();
+            
         }
 
         // PUT: api/UpdateChildTaskVP/5
@@ -37,6 +54,11 @@ namespace Updating_Child_Task_With_Story_Details_WebAPI.Controllers.API
         // DELETE: api/UpdateChildTaskVP/5
         public void Delete(int id)
         {
+        }
+
+        public Task GetParentStory(ModelProps props)
+        {
+            return null;
         }
     }
 }
---
+++ Updating-Child-Task-With-Story-Details-WebAPI/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Updating_Child_Task_With_Story_Details_WebAPI.WebApiApplication" Language="C#" %>
